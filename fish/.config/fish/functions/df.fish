613e2a1510ec9fe77b1ab556b8a94c3736d27032
John Helliwell <john.helliwell@gmail.com>
2024-12-18 14:31:32 +1100

    Improvements to workflow

--- fish/.config/fish/functions/df.fish
+++ fish/.config/fish/functions/df.fish
@@ -1,4 +1,4 @@
-function df --wraps=pydf --description 'alias df pydf'
-  pydf $argv
+function df --wraps='/bin/df -x tmpfs -x devtmpfs -h' --description 'alias df /bin/df -x tmpfs -x devtmpfs -h'
+  /bin/df -x tmpfs -x devtmpfs -h $argv
         
 end
