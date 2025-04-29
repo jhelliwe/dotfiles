c8839ed1f8b017e6eb2783fb13768640c2c2502e
John Helliwell <john.helliwell@gmail.com>
2025-04-29 20:08:33 +1000

    Fixed Now Playing when not playing music

--- Hyprland/hyprland/bin/getspot.sh
+++ Hyprland/hyprland/bin/getspot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-artist=`python ~/hyprland/bin/getspot.py  | awk ' /^xesam:artist/ { print } ' | sed "s/xesam:artist dbus.Array..dbus.String..\(.*\).... signature=dbus.Signature..s.., variant_level=1./\1/g"`
-track=`python ~/hyprland/bin/getspot.py | tail -1`
-echo "Playing: $artist - $track" | sed "s/&/ and /g"
+if pidof -q spotify 
+then
+	artist=`python ~/hyprland/bin/getspot.py  | awk ' /^xesam:artist/ { print } ' | sed "s/xesam:artist dbus.Array..dbus.String..\(.*\).... signature=dbus.Signature..s.., variant_level=1./\1/g"`
+	track=`python ~/hyprland/bin/getspot.py | tail -1` 
+	echo "Playing: $artist - $track" | sed "s/&/ and /g"
+else
+	echo Hyprland
+fi
