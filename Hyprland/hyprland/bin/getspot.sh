a1d8f30d983e24a076b591f187deb009f5f96054
John Helliwell <john.helliwell@gmail.com>
2025-04-29 16:49:00 +1000

    Added Now Playing to waybar

---
+++ Hyprland/hyprland/bin/getspot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+artist=`python ~/hyprland/bin/getspot.py  | awk ' /^xesam:artist/ { print } ' | sed "s/xesam:artist dbus.Array..dbus.String..\(.*\).... signature=dbus.Signature..s.., variant_level=1./\1/g"`
+track=`python ~/hyprland/bin/getspot.py | tail -1`
+echo "Playing: $artist - $track" | sed "s/&/ and /g"
